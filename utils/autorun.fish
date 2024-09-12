157d9c0ad4f7b2648f94ab677dc363622a92ae5e
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-09-12 15:47:19 -0400

    UPDATE autorun

--- utils/autorun.fish
+++ utils/autorun.fish
@@ -4,19 +4,27 @@ function autorun
 
     # Set up a GPT prompt to analyze the project and determine the appropriate run command
     set run_prompt "Based on the following project files and content, please suggest a command to run the project: $project_context.
-    Only respond with the exact command to run the project.In addition: $argv "
+    Only respond with the exact command to run the project. In addition: $argv "
 
     # Get the run command using GPT
     set run_command (g "$run_prompt")
 
     e $run_command
+    e $run_command | clip
 
     set response
-    read --prompt-str "Do you want to run this command\? \(y/n\): " response
+    if test (count $argv) -gt 0
+        if test $argv[1] = "y"
+            set response "y"
+        else
+            set response "n"
+        end
+    else
+        read --prompt-str "Do you want to run this command? (y/n): " response
+    end
 
     if test "$response" = "y"
         echo "Running the application..."
         eval $run_command
     end
-
 end
